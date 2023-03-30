10e003c8321baa95aa359870d09eb0fc341c3a89
KiewYanHui <b022120024@student.utem.edu.my>
2023-03-30 15:54:18 +0800

    lecture2

---
+++ quiz1.js
@@ -0,0 +1,20 @@
+let message = "benr"
+console.log(message,typeof message)
+
+message=123
+console.log(message,typeof message)
+
+let str=2
+let num=3
+let course = parseInt(str) + num
+console.log(course,typeof course)
+
+var array=["s",50,"k"]
+console.log(array[1],typeof array)
+
+var data={name:"q",age:22,course:"BENR"}
+console.log(data.name,typeof data)
+
+var data2=[{name:"q",message:"wah"},{name:"s",message:"wow"},
+            {name:"k",message:"wowwwwww"}]
+console.log(data2[0].name)
